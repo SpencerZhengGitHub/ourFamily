4ab7dbd7a97eff1fef220392367b58def7b60639
spencerzheng <spencerzheng163@163.com>
2019-05-11 09:57:35 +0800

    项目

---
+++ ourfamily_run/src/main/java/com/ourfamily/ourfamily_run/OurfamilyRunApplication.java
@@ -0,0 +1,13 @@
+package com.ourfamily.ourfamily_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurfamilyRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurfamilyRunApplication.class, args);
+    }
+
+}
